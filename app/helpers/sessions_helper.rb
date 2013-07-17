1769884a34bec037ff00c0c5183a9b20b4f181a4
sat <sat@ubuntu.ubuntu-domain>
2013-07-16 21:26:15 -0700

    with remember password

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,8 +13,7 @@ module SessionsHelper
   end
 
   def current_user
-    @current_user ||= nil
-    #User.find_by_remember_token(cookies[:remember_token])
+    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
 
   def sign_out
